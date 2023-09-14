57d7750867a79ee142f42db0dc374160d47cf314
IWhitebird <115157819+IWhitebird@users.noreply.github.com>
2023-09-14 14:26:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ module.exports = (app) => {
           "base64"
         ).toString("utf-8");
 
-        if (context.payload.pull_request.body.includes("/explain")) {
+        if (context.payload.pull_request.body !== null && context.payload.pull_request.body.includes("/explain")) {
           const explanation = await getExplanation(codeChanges);
 
           context.octokit.issues.createComment({
@@ -50,7 +50,7 @@ module.exports = (app) => {
           });
         }
 
-        if (context.payload.pull_request.body.includes("/execute")) {
+        if (context.payload.pull_request.body !== null && context.payload.pull_request.body.includes("/execute")) {
           const output = await getOutput(codeChanges, file.filename);
 
           if(output && output.length > 0){
@@ -60,12 +60,81 @@ module.exports = (app) => {
               issue_number: pullNumber,
               body: `Output for changes in ${file.filename}:\n${output}\n`,
             });
-
           }
         }
       }
     }
   );
+
+  app.on(["pull_request_review_comment"], async (context) => {
+    
+    const repoName = context.payload.repository.name;
+    const repoOwner = context.payload.repository.owner.login;
+    const pullNumber = context.payload.pull_request.number;
+    const comment = context.payload.comment.body;
+
+    const files = await context.octokit.pulls.listFiles({
+      repo: repoName,
+      owner: repoOwner,
+      pull_number: pullNumber,
+    });
+
+    if (!files || files.data.length === 0) {
+      context.log.warn("No files found in the pull request.");
+      return;
+    }
+
+
+    for (const file of files.data) {
+
+      const fileContent = await context.octokit.repos.getContent({
+        repo: repoName,
+        owner: repoOwner,
+        path: file.filename,
+        ref: context.payload.pull_request.head.sha,
+      });
+
+      const codeChanges = Buffer.from(
+        fileContent.data.content,
+        "base64"
+      ).toString("utf-8");
+
+
+      if (comment !== null && comment.includes("/explain") && file.filename === context.payload.comment.path) {
+        
+        const explanation = await getExplanation(codeChanges);
+
+        context.octokit.rest.pulls.createReviewComment({
+          repo: repoName,
+          owner: repoOwner,
+          pull_number: pullNumber,
+          body: `Explanation for changes in ${file.filename}:\n${explanation}\n`,
+          commit_id: context.payload.comment.commit_id,
+          path: context.payload.comment.path,
+          position: context.payload.comment.position,
+        });
+
+      }
+
+      if (comment !== null && comment.includes("/execute") && file.filename === context.payload.comment.path) {
+        const output = await getOutput(codeChanges, file.filename);
+        
+        if(output && output.length > 0){
+
+          context.octokit.rest.pulls.createReviewComment({
+            repo: repoName,
+            owner: repoOwner,
+            pull_number: pullNumber,
+            body: `Output for changes in ${file.filename}:\n${output}\n`,
+            commit_id: context.payload.comment.commit_id,
+            path: context.payload.comment.path,
+            position: context.payload.comment.position,
+          });          
+
+        }
+      }
+    }
+  });
 
   async function getOutput(code, filename) {
     try {
@@ -93,26 +162,27 @@ module.exports = (app) => {
 
   async function getExplanation(code) {
     try {
-      const data = {
-        model: "pai-001-light-beta",
-        prompt: `Explain this code to me:\n' + ${code} + '\n\n`,
-        temperature: 0.7,
-        max_tokens: 256,
-        stop: ["Human:", "AI:"],
-      };
+      return "yolo";
+      // const data = {
+      //   model: "pai-001-light-beta",
+      //   prompt: `Explain this code to me:\n' + ${code} + '\n\n`,
+      //   temperature: 0.7,
+      //   max_tokens: 256,
+      //   stop: ["Human:", "AI:"],
+      // };
 
-      const headers = {
-        Authorization: `Bearer ${process.env.FREE_LLM}`,
-        "Content-Type": "application/json",
-      };
+      // const headers = {
+      //   Authorization: `Bearer ${process.env.FREE_LLM}`,
+      //   "Content-Type": "application/json",
+      // };
 
-      const response = await axios.post(
-        "https://api.pawan.krd/v1/completions",
-        data,
-        { headers }
-      );
+      // const response = await axios.post(
+      //   "https://api.pawan.krd/v1/completions",
+      //   data,
+      //   { headers }
+      // );
 
-      return response.data.choices[0].text;
+      // return response.data.choices[0].text;
     } catch (err) {
       console.error(err);
     }
