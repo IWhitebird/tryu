cd0330084330ea819bae73d5aa74982616538659
IWhitebird <115157819+IWhitebird@users.noreply.github.com>
2023-09-15 23:30:30 +0530

    Commenting

--- app.js
+++ app.js
@@ -3,33 +3,41 @@
  * @param {import('probot').Probot} app
  */
 
-// const OpenAI = require("openai");
+// Import necessary modules
+// const OpenAI = require("openai"); // If you plan to use the OpenAI API
 const axios = require("axios");
 const result = require("./data");
 
+// Export the Probot app function
 module.exports = (app) => {
-
+  // Log a message when the Probot app starts
   app.log("PROBOT WEBHOOK");
 
+  // Define event listeners for pull requests
   app.on(
     ["pull_request.opened", "pull_request.reopened", "pull_request.edited"],
     async (context) => {
+      // Extract information about the pull request
       const repoName = context.payload.repository.name;
       const repoOwner = context.payload.repository.owner.login;
       const pullNumber = context.payload.number;
 
+      // Get the list of files in the pull request
       const files = await context.octokit.pulls.listFiles({
         repo: repoName,
         owner: repoOwner,
         pull_number: pullNumber,
       });
 
+      // Check if there are any files in the pull request
       if (!files || files.data.length === 0) {
         context.log.warn("No files found in the pull request.");
         return;
       }
 
+      // Iterate through the files in the pull request
       for (const file of files.data) {
+        // Get the content of the file
         const fileContent = await context.octokit.repos.getContent({
           repo: repoName,
           owner: repoOwner,
@@ -37,17 +45,21 @@ module.exports = (app) => {
           ref: context.payload.pull_request.head.sha,
         });
 
+        // Convert the file content from base64 to UTF-8
         const codeChanges = Buffer.from(
           fileContent.data.content,
           "base64"
         ).toString("utf-8");
 
+        // Check if the pull request body includes "/explain"
         if (
           context.payload.pull_request.body !== null &&
           context.payload.pull_request.body.includes("/explain")
         ) {
+          // Get an explanation for the code changes
           const explanation = await getExplanation(codeChanges);
 
+          // Create a comment with the explanation
           context.octokit.issues.createComment({
             repo: repoName,
             owner: repoOwner,
@@ -56,12 +68,15 @@ module.exports = (app) => {
           });
         }
 
+        // Check if the pull request body includes "/execute"
         if (
           context.payload.pull_request.body !== null &&
           context.payload.pull_request.body.includes("/execute")
         ) {
+          // Get the output of executing the code changes
           const output = await getOutput(codeChanges, file.filename);
 
+          // If there is output, create a comment with the output
           if (output && output.length > 0) {
             context.octokit.issues.createComment({
               repo: repoName,
@@ -75,24 +90,30 @@ module.exports = (app) => {
     }
   );
 
+  // Define event listener for pull request review comments
   app.on(["pull_request_review_comment"], async (context) => {
+    // Extract information about the pull request and comment
     const repoName = context.payload.repository.name;
     const repoOwner = context.payload.repository.owner.login;
     const pullNumber = context.payload.pull_request.number;
     const comment = context.payload.comment.body;
 
+    // Get the list of files in the pull request
     const files = await context.octokit.pulls.listFiles({
       repo: repoName,
       owner: repoOwner,
       pull_number: pullNumber,
     });
 
+    // Check if there are any files in the pull request
     if (!files || files.data.length === 0) {
       context.log.warn("No files found in the pull request.");
       return;
     }
 
+    // Iterate through the files in the pull request
     for (const file of files.data) {
+      // Get the content of the file
       const fileContent = await context.octokit.repos.getContent({
         repo: repoName,
         owner: repoOwner,
@@ -100,18 +121,22 @@ module.exports = (app) => {
         ref: context.payload.pull_request.head.sha,
       });
 
+      // Convert the file content from base64 to UTF-8
       const codeChanges = Buffer.from(
         fileContent.data.content,
         "base64"
       ).toString("utf-8");
 
+      // Check if the comment includes "/explain" and the file matches
       if (
         comment !== null &&
         comment.includes("/explain") &&
         file.filename === context.payload.comment.path
       ) {
+        // Get an explanation for the code changes
         const explanation = await getExplanation(codeChanges);
 
+        // Create a review comment with the explanation
         context.octokit.rest.pulls.createReviewComment({
           repo: repoName,
           owner: repoOwner,
@@ -123,13 +148,16 @@ module.exports = (app) => {
         });
       }
 
+      // Check if the comment includes "/execute" and the file matches
       if (
         comment !== null &&
         comment.includes("/execute") &&
         file.filename === context.payload.comment.path
       ) {
+        // Get the output of executing the code changes
         const output = await getOutput(codeChanges, file.filename);
 
+        // If there is output, create a review comment with the output
         if (output && output.length > 0) {
           context.octokit.rest.pulls.createReviewComment({
             repo: repoName,
@@ -145,11 +173,14 @@ module.exports = (app) => {
     }
   });
 
+  // Function to get the output of executing code
   async function getOutput(code, filename) {
     try {
+      // Extract the file type and version from the result object
       const filetype = filename.split(".").pop();
       const version = result[filetype];
 
+      // Execute the code using an external API (emkc.org)
       const output = await axios.post(
         "https://emkc.org/api/v2/piston/execute",
         {
@@ -169,8 +200,10 @@ module.exports = (app) => {
     }
   }
 
+  // Function to get an explanation for code
   async function getExplanation(code) {
     try {
+      // Define data and headers for the explanation request
       const data = {
         model: "pai-001-light-beta",
         prompt: `Explain this code to me:\n' + ${code} + '\n\n`,
@@ -184,6 +217,7 @@ module.exports = (app) => {
         "Content-Type": "application/json",
       };
 
+      // Send a request to an external API (api.pawan.krd)
       const response = await axios.post(
         "https://api.pawan.krd/v1/completions",
         data,
@@ -196,7 +230,7 @@ module.exports = (app) => {
     }
   }
 
-  //GPT API IF AVIALABLE
+  // Uncomment this section if you plan to use the OpenAI API for explanation
   // async function getExplanation(code){
   //   try{
   //     const myopenai = new OpenAI(process.env.OPENAI_API_KEY);
